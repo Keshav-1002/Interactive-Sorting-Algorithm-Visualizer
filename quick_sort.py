0c40a797d5df74b38b20062c79282f4d6785e029
Keshav Singhal <keshavsinghal1002@gmail.com>
2025-06-18 17:14:52 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,32 +1,33 @@
 import time
 
-def partition(arr, low, high, draw_data, get_speed):
-    i = (low - 1)
-    pivot = arr[high] 
-
-    for j in range(low, high):
-        draw_data(arr, digit=j, digit2=high, optional_color='white', var1=low, var2=high)
-        time.sleep(get_speed())
-        
-        if arr[j] <= pivot: 
-            i = i + 1
-            arr[i], arr[j] = arr[j], arr[i]
-            draw_data(arr, digit=j, digit2=i, optional_color='red', var1=low, var2=high)
-            
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    draw_data(arr, digit=i+1, digit2=high, optional_color='red', var1=low, var2=high)
-    
-    time.sleep(get_speed())
-    return i + 1
-
-def quickSortA(arr, low, high, draw_data, get_speed):
-    
-    if low < high:
-        pi = partition(arr, low, high, draw_data, get_speed)
-
-        quickSortA(arr, low, pi - 1, draw_data, get_speed)
-        quickSortA(arr, pi + 1, high, draw_data, get_speed)
-
 def quick_sort(data, draw_data, get_speed):
-    quickSortA(data, 0, len(data) - 1, draw_data, get_speed)
-    draw_data(data, optional_color='cyan')
+    
+    def _quick_sort(data, low, high):
+        
+        if low < high:
+            pi = partition(data, low, high)
+            _quick_sort(data, low, pi - 1)
+            _quick_sort(data, pi + 1, high)
+    
+    def partition(data, low, high):
+        pivot = data[high]
+        i = low - 1
+        
+        for j in range(low, high):
+            draw_data(data, optional_color='white', digit=j, digit2=high, end=high)
+            time.sleep(get_speed())
+            
+            if data[j] <= pivot:
+                i += 1
+                data[i], data[j] = data[j], data[i]
+                draw_data(data, optional_color='red', digit=i, digit2=j, end=high)
+                time.sleep(get_speed())
+        
+        data[i + 1], data[high] = data[high], data[i + 1]
+        draw_data(data, optional_color='red', digit=i+1, digit2=high, end=high)
+        time.sleep(get_speed())
+        return i + 1
+    
+    _quick_sort(data, 0, len(data) - 1)
+    draw_data(data, end=len(data)-1)
+    
