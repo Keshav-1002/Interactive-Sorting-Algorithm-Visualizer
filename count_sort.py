c988952c70ec3450784176d05fe37065b6fa1219
Keshav Singhal <keshavsinghal1002@gmail.com>
2025-06-18 17:13:05 +0530

    Update count_sort.py

--- count_sort.py
+++ count_sort.py
@@ -1,5 +1,31 @@
 import time
 
+def count_sort(data, draw_data, get_speed):
+    max_val = max(data)
+    count = [0] * (max_val + 1)
+    output = [0] * len(data)
+
+    for i in range(len(data)):
+        count[data[i]] += 1
+        draw_data(data, optional_color='white', digit=i, end=-1)
+        time.sleep(get_speed())
+
+    for i in range(1, len(count)):
+        count[i] += count[i - 1]
+
+    for i in range(len(data) - 1, -1, -1):
+        output[count[data[i]] - 1] = data[i]
+        count[data[i]] -= 1
+        draw_data(data, optional_color='red', digit=i, end=-1)
+        time.sleep(get_speed())
+
+    for i in range(len(data)):
+        data[i] = output[i]
+        draw_data(data, optional_color='red', digit=i, end=i)
+        time.sleep(get_speed())
+
+    draw_data(data, end=len(data)-1)
+    
 def count_sort(arr, drawdata, speed):
     time_gap = speed()
 
@@ -31,3 +57,4 @@ def count_sort(arr, drawdata, speed):
         arr[i] = output[i]
         drawdata(arr, optional_color='green', digit=i)
         time.sleep(time_gap)
+        
