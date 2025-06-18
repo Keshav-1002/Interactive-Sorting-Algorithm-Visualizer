cf2ba4e78eae343b4fc5f1e123ed7eb2a865b827
Keshav Singhal <keshavsinghal1002@gmail.com>
2025-06-18 17:15:33 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -5,17 +5,17 @@ def selection_sort(data, draw_data, get_speed):
     
     for i in range(n):
         min_idx = i
-        draw_data(data, end=i-1, optional_color='white', digit=i)
-        time.sleep(get_speed())
         
         for j in range(i + 1, n):
-            draw_data(data, end=i-1, optional_color='white', digit=j, digit2=i)
+            draw_data(data, optional_color='white', digit=j, digit2=min_idx, end=i-1)
             time.sleep(get_speed())
             
-            if data[min_idx] > data[j]:
+            if data[j] < data[min_idx]:
                 min_idx = j
-                draw_data(data, end=i-1, optional_color='red', digit=j, digit2=i)
                 
         data[i], data[min_idx] = data[min_idx], data[i]
-        
-    draw_data(data, optional_color='cyan') 
+        draw_data(data, optional_color='red', digit=i, digit2=min_idx, end=i)
+        time.sleep(get_speed())
+
+    draw_data(data, end=n-1)
+    
